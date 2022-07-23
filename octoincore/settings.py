7b4d44fa4f55c4291594e67a9098c4c7542f8e95
0x70 <greezygreed@gmail.com>
2022-07-24 01:48:40 +0200

    mathfilters for calculations in template rendering

--- octoincore/settings.py
+++ octoincore/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     # third party apps
+    "mathfilters",
     # local apps
     "octoincore.dashboard",
     "octoincore.console",
