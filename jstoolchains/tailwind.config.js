e78c4d61f25a4063dbbb5c6862c565a701335458
0x70 <greezygreed@gmail.com>
2023-12-09 18:56:38 +0100

    js package updates

--- jstoolchains/tailwind.config.js
+++ jstoolchains/tailwind.config.js
@@ -2,11 +2,16 @@
 module.exports = {
   content: ["../**/templates/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        "[auto,auto,1fr]": "auto auto 1fr",
+      },
+    },
   },
   plugins: [
     require("@tailwindcss/forms"),
     require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
     require("daisyui"),
   ],
 };
