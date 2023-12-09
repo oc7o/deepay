42e21563cf6552bc6ed5bf6dc4af7ac5428f546e
0x70 <greezygreed@gmail.com>
2023-12-09 19:01:01 +0100

    updated landing page and layout

--- deepay/apps/landing/views.py
+++ deepay/apps/landing/views.py
@@ -1,17 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import ListView, TemplateView
 
 from deepay.apps.inventory.models import Product
 
 
-class LandingView(TemplateView):
+class LandingView(ListView):
     template_name = "landing/landing.html"
+    context_object_name = "products"
+    model = Product
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["products"] = Product.objects.all()
-
-        print(
-            1, Product.objects.first().inventories.first().media_files.first().image.url
-        )
-        return context
+    paginate_by = 8
